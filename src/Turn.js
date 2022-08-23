1430f1b116c8d9c3f21539d58a771facf133d475
Emily Miles <ms.emily.j.miles@gmail.com>
2022-08-22 23:01:18 -0400

    Add Turn class file

---
+++ src/Turn.js
@@ -0,0 +1,32 @@
+class Turn {
+    constructor(userGuess, currentCard) {
+        this.guess = userGuess;
+        this.card = currentCard;
+    };
+
+    returnGuess() {
+        return this.guess
+    };
+
+    returnCard() {
+        return this.card;
+    };
+
+    evaluateGuess() {
+        if (this.card.correctAnswer !== this.guess) {
+            return false
+        } else {
+            return true
+        };
+    };
+
+    giveFeedback() {
+        if (this.evaluateGuess() === false) {
+            return 'Incorrect!'
+        } else {
+            return 'Correct!'
+        };
+    };
+};
+
+module.exports = Turn;
