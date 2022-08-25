6b2c7f8c3625126064cb52953da4c0a8ba3b44a7
Emily Miles <ms.emily.j.miles@gmail.com>
2022-08-25 16:51:58 -0400

    Refactor Game class with ES6 arrow functions

--- src/Game.js
+++ src/Game.js
@@ -13,37 +13,25 @@ class Game {
     this.round = {};
   }
 
-  printMessage(deck) {
-    console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
-  }
+  printMessage = (deck) => { console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
+-----------------------------------------------------------------------`) };
 
-  printQuestion(round) {
-      util.main(round);
-  }
+  printQuestion = (round) => { util.main(round) };
 
-  createCards() {
+  createCards = () => {
     this.cards = prototypeQuestions.map(card => new Card(card.id, card.question, card.answers, card.correctAnswer))
     return this.cards
-  }
+  };
 
-  createDeck() {
-    this.deck = new Deck(this.createCards())
-    return this.deck
-  }
+  createDeck = () =>{ return this.deck = new Deck(this.createCards()) };
 
-  newRound() {
-    this.round = new Round(this.createDeck())
-    return this.round
-  }
+  newRound = () => { return this.round = new Round(this.createDeck()) };
 
-  startGame() {
-    // this.createCards();
-    // this.createDeck();
+  startGame = () => {
     this.newRound();
     this.printMessage(this.deck, this.round);
     this.printQuestion(this.round)
-  }
+  };
 }
 
 module.exports = Game;
